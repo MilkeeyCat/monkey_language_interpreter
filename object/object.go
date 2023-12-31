2ef7750032c43fe341e1c28d57e985f2ceaef557
MilkeeyCat <40633857+MilkeeyCat@users.noreply.github.com>
2023-12-31 14:58:57 +0200

    feat: function evaluation

--- object/object.go
+++ object/object.go
@@ -1,6 +1,12 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+
+	"github.com/Milk-Interpreters/milk_interpreter_go/ast"
+)
 
 type ObjectType string
 
@@ -10,6 +16,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 type Object interface {
@@ -70,26 +77,27 @@ func (e *Error) Type() ObjectType {
 	return ERROR_OBJ
 }
 
-type Environment struct {
-	store map[string]Object
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
 
-	return &Environment{
-		store: s,
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
 	}
-}
 
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
 
-	return obj, ok
-}
-
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
-
-	return val
+	return out.String()
 }
